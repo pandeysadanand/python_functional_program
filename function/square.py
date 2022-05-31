0058d43ba710c833a74af5095a36d1a6b3d7ecc0
sadanand pandey <pandeysadanad75@gmail.com>
2022-05-31 16:33:19 +0530

    Added: finding square of given number

---
+++ function/square.py
@@ -0,0 +1,12 @@
+
+def square(number):
+    """
+    Write a function to take number as input and print its square value
+    :param number: number
+    :return: none
+    """
+    print("Square of", number, "is", number * number)
+
+
+square(5)
+square(6)
