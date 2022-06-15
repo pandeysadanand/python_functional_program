96efd12038c0a587b6021e28679c3b8e8bf3b2ce
pandeysadanand <100331524+pandeysadanand@users.noreply.github.com>
2022-06-15 20:49:48 +0530

    [Sadanand Pandey]Added: username check

--- FunctionalPrograms/FunctionalPrograms/user_name.py
+++ FunctionalPrograms/FunctionalPrograms/user_name.py
@@ -2,7 +2,7 @@
     @Author: SADANAND PANDEY
     @Date: 2022-06-02 09:36:00
     @Last Modified by: SADANAND PANDEY
-    @Last Modified time: 2022-06-02 09:50:00
+    @Last Modified time: 2022-06-02 09:55:00
     @Title : Python program to User Input and Replace String Template “Hello <<UserName>>, How are you?”
 """
 # Taking user-name
